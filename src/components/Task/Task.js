e3339e508144e980b8fd0e2142f478c53705aeb7
quangtung20 <trangquangtung20111999@gmail.com>
2021-10-07 23:00:11 +0700

    architechture

---
+++ src/components/Task/Task.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Task.scss';
+
+function Task() {
+    return (
+        <li className="task-item">
+            <img src="https://vnreview.vn/image/16/81/51/1681516.jpg?t=1499848476274"></img>
+            Quang Tung
+        </li>
+    )
+}
+export default Task
