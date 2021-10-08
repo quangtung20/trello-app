6eb35ee8c9dfbea5652ea3b6bf1e6eb70c2afed7
quangtung20 <trangquangtung20111999@gmail.com>
2021-10-08 23:22:07 +0700

    init sample data

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.scss';
 import AppBar from './components/AppBar/AppBar';
 import BoardBar from './components/BoadBar/BoardBar';
 import BoardContent from './components/BoardContent/BoardContent';
-
+// khi cai thu vien gap loi nao thi cu chay npm audit fix
 
 function App() {
   return (
--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Column from "../Column/Column";
-import './BoardContent.scss';
+import { mapOrder } from "../../ultilities/sorts";
+import "./BoardContent.scss";
+import { initialData } from "../../actions/initialData";
+import { isEmpty } from "lodash";
 
 function BoardContent() {
+    const [board, setBoard] = useState({});
+    const [columns, setColumns] = useState([]);
+    useEffect(() => {
+        const boardFromDB = initialData.boards.find(
+            (board) => board.id === "board-1"
+        );
+        if (boardFromDB) {
+            setBoard(boardFromDB);
+            setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, 'id'));
+        }
+    }, []);
+
+    if (isEmpty(board)) {
+        return (
+            <div className="not-found" style={{ padding: "10px", color: "white" }}>
+                Board not found !
+            </div>
+        );
+    }
+
     return (
         <div className="board-content">
-            <Column />
-            <Column />
-            <Column />
-            <Column />
-            <Column />
+            {columns.map((column, index) => (
+                <Column key={index} column={column} />
+            ))}
         </div>
-    )
+    );
 }
-export default BoardContent
+export default BoardContent;
