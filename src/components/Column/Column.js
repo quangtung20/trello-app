cce0f1609f2f86f500b1292e1418024b58da0dc7
quangtung20 <trangquangtung20111999@gmail.com>
2021-10-12 10:42:51 +0700

    edit remove column

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,28 +1,80 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Card from "../Card/Card";
 import "./Column.scss";
 import { mapOrder } from "../../ultilities/sorts";
 import { Container, Draggable } from "react-smooth-dnd";
+import { Dropdown, Form } from 'react-bootstrap';
+import ConfirmModal from "../Common/ConfirmModal";
+import { MODAL_ACTION_CLOSE, MODAL_ACTION_CONFIRM } from '../../ultilities/constants';
+import { saveContentAfterPressEnter, selectAllInText } from '../../ultilities/contentEditable';
 
 function Column(props) {
-    const { column, onCardDrop } = props;
-    const cards = mapOrder(column.cards, column.cardOrder, "id");
+    const { column, onCardDrop, onUpdateColumn } = props;
+    const [showConfirmModal, setShowConfirmModal] = useState(false);
+    const [columnTitle, setColumnTitle] = useState('');
+    useEffect(() => {
+        setColumnTitle(column.title);
+    }, [column.title]);
+
+    const toggleShowConfirmModal = () => setShowConfirmModal(!showConfirmModal);
+    const cards = mapOrder(column.cards, column.cardOrder, 'id');
+    const onConfirmModalAction = (type) => {
+        if (type === MODAL_ACTION_CONFIRM) {
+            const newColumn = {
+                ...column,
+                _destroy: true
+            }
+
+            onUpdateColumn(newColumn);
+        }
+        toggleShowConfirmModal();
+    }
+
+    const handleColumnTitleBlur = () => {
+        const newColumn = {
+            ...column,
+            title: columnTitle
+        }
+        onUpdateColumn(newColumn);
+    }
+
+
+
+    const handleColumnTitleChange = useCallback((e) => setColumnTitle(e.target.value), [])
 
     return (
         <div className="column">
-            <header className="column-drag-handle">{column.title}</header>
-            <div className="Card-list">
-                <Container
+            <header className="column-drag-handle">
+                <div className="column-title">
 
-                    // onDragStart={(e) => console.log("drag started", e)}
-                    // onDragEnd={(e) => console.log("drag end", e)}
-                    // onDragEnter={() => {
-                    //     console.log("drag enter:", column.id);
-                    // }}
-                    // onDragLeave={() => {
-                    //     console.log("drag leave:", column.id);
-                    // }}
-                    // onDropReady={(p) => console.log("Drop ready: ", p)}
+                    <Form.Control size="sm" type="text"
+                        className="quangtung-content-editable"
+                        onChange={handleColumnTitleChange}
+                        onBlur={handleColumnTitleBlur}
+                        onKeyDown={saveContentAfterPressEnter}
+                        value={columnTitle}
+                        onClick={selectAllInText}
+                        onMouseDown={e => e.preventDefault()}
+                        spellCheck="false"
+
+                    />
+                </div>
+                <div className="column-dropdown-actions">
+                    <Dropdown>
+                        <Dropdown.Toggle variant="success" id="dropdown-basic" size="sm" className="dropdown-btn" />
+
+                        <Dropdown.Menu>
+                            <Dropdown.Item >Add card ...</Dropdown.Item>
+                            <Dropdown.Item onClick={toggleShowConfirmModal}>Remove Column</Dropdown.Item>
+                            <Dropdown.Item >Move all cards</Dropdown.Item>
+                            <Dropdown.Item >Archie all cards</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </div>
+            </header>
+            <div className="Card-list">
+
+                <Container
                     orientation="vertical"
                     groupName="tung-col"
                     onDrop={dropResult => onCardDrop(column.id, dropResult)}
@@ -50,6 +102,12 @@ function Column(props) {
                     <i className="fa fa-plus"> Add another card</i>
                 </div>
             </footer>
+            <ConfirmModal
+                show={showConfirmModal}
+                onAction={onConfirmModalAction}
+                title='remove column'
+                content={`Are you sure to remove <strong>${column.title}</strong>!`}
+            />
         </div>
     );
 }
